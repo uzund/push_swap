bbeefdc5805813ccc99de4c698de815cdab5916f
Davut UZUN <duzun@10.0.2.15>
2023-01-20 20:10:20 +0300

    Update..

---
+++ srcs/ft_check.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 20:04:28 by duzun             #+#    #+#             */
+/*   Updated: 2023/01/20 20:05:55 by duzun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_check_number(const char *s)
+{
+	int	i;
+	int	pluscount;
+	int	minuscount;
+
+	i = -1;
+	pluscount = 0;
+	minuscount = 0;
+	while (s[++i] != '\0')
+	{
+		if ((!ft_isdigit(s[i]) && s[i] != '-' && s[i] != '+') && !ft_isin(s[i]))
+			return (0);
+		if (s[i] == '+' || s[i] == '-')
+		{
+			if (i == 0 || !ft_isdigit(s[i - 1]))
+			{
+				if (s[i] == '+')
+					pluscount++;
+				else
+					minuscount++;
+			}
+			else
+				return (0);
+		}
+	}
+	return (!(pluscount > 1 || minuscount > 1));
+}
+
+int	ft_check_duplicate(char **arraytmp)
+{
+	char	**array;
+	int		i;
+	int		j;
+
+	array = arraytmp;
+	i = -1;
+	while (array[++i])
+	{
+		j = i + 1;
+		while (array[j])
+		{
+			if (ft_strcmp(array[i], array[j]) == 0)
+				return (0);
+			j++;
+		}
+	}
+	return (1);
+}
+
+int	ft_check_minmax(char **s)
+{
+	char	**arg;
+	int		check;
+	int		i;
+	double	minmax;
+
+	arg = s;
+	check = 0;
+	i = -1;
+	while (arg[++i])
+	{
+		minmax = ft_atof(&arg[i][0]);
+		printf("minmax değeri :%.0f\n", minmax);
+		if (minmax < -2147483648 || minmax > 2147483647)
+		{
+			printf("limitlerin üzerinde değer\n");
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	ft_check_sign(const char *s)
+{
+	int		i;
+	int		count;
+	char	c;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		c = s[i];
+		if ((c != ' ' && c != '-' && !(c >= '0' && c <= '9')) && \
+			(c != ' ' && c != '+' && !(c >= '0' && c <= '9')))
+			return (0);
+		if (((c == '-' && !(s[i + 1] >= '0' && s[i + 1] <= '9')) || \
+			(i > 0 && c == '-' && !(s[i - 1] == ' '))) || \
+			((c == '+' && !(s[i + 1] >= '0' && s[i + 1] <= '9')) || \
+				(i > 0 && c == '+' && !(s[i - 1] == ' '))))
+			return (0);
+		if (c >= '0' && c <= '9')
+			count++;
+		i++;
+	}
+	if (count == 0)
+		return (0);
+	return (1);
+}
--- srcs/ft_lst.c
+++ srcs/ft_lst.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lst.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: duzun <davut@uzun.ist>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:39:48 by duzun             #+#    #+#             */
-/*   Updated: 2023/01/18 22:47:54 by duzun            ###   ########.fr       */
+/*   Updated: 2023/01/20 20:08:47 by duzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,54 @@ void	ft_lstadd_back(t_list **stack, t_list *new)
 		(*stack)->next = NULL;
 	}
 }
+
+void	ft_lstadd_front(t_list **stack, t_list *new)
+{
+	new->next = *stack;
+	*stack = new;
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*tmp;
+
+	tmp = lst;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+			return (tmp);
+	}
+	return (tmp);
+}
+
+t_list	*ft_lstnew(int data)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->index = -1;
+	new->next = NULL;
+	return (new);
+}
+
+int	ft_lstsize(t_list *lst)
+{
+	size_t	cont;
+	t_list	*tmp;
+
+	tmp = lst;
+	cont = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		cont++;
+	}
+	return (cont);
+}
+
+
+
